73d66f7e61142531212a76641e6fc9cd18c4c6bc
John Brandenburg <jbrandenburg@forumone.com>
2015-05-28 11:09:30 -0400

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+class jpeg-optimize ($release = "1.4.3") {
+
+  $url = "https://github.com/tjko/jpegoptim/archive/RELEASE.${release}.tar.gz"
+  $filename = "jpegoptim-RELEASE.${release}.tar.gz"
+
+
+  package {"libjpeg-turbo-devel":
+    ensure => installed,
+  }
+
+  exec {"jpeg-optimize::download":
+    command => "wget -O ${filename} --directory-prefix=/tmp/vagrant-cache ${url}",
+    path => "/usr/bin/"
+    creates => "/tmp/vagrant-cache/${filename}",
+    timeout => 4800,
+  }
+
+  exec {"jpeg-optimize::extract":
+    command => "tar -zxvf /tmp/vagrant-cache/${filename} -C /opt",
+    path    => "/bin",
+    require => [Exec["jpeg-optimize::download"]],
+    creates => "/opt/jpegoptim-RELEASE.${release}/README",
+  }
+
+  exec {"jpeg-optimize::configure":
+    command => "/opt/jpegoptim-RELEASE.${release}/configure",
+    require => "jpeg-optimize::extract",
+    creates => "/opt/jpegoptim-RELEASE.${release}/Makefile"
+  }
+
+  exec {"jpeg-optimize::make"
+    command => "make -C /opt/jpegoptim-RELEASE.${release}/",
+    path => "/usr/bin",
+    require => "jpeg-optimize::configure",
+    creates => "/opt/jpegoptim-RELEASE.${release}/jpegoptim"
+  }
+
+}
+
