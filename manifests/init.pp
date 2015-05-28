5715cee48150a24caf42d797a91449c87ed18991
John Brandenburg <jbrandenburg@forumone.com>
2015-05-28 15:43:59 -0400

    Removing -O option in wget

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,10 +8,10 @@ class jpeg-optimize::optimize($release = "1.4.3") {
     ensure => installed,
   }
 
-  notify {"wget -O ${filename} --directory-prefix=/tmp/vagrant-cache ${url}": }
+  notify {"wget --directory-prefix=/tmp/vagrant-cache ${url}": }
 
   exec { "jpeg-optimize::optimize::download":
-    command => "wget -O ${filename} --directory-prefix=/tmp/vagrant-cache ${url}",
+    command => "wget --directory-prefix=/tmp/vagrant-cache ${url}",
     path => "/usr/bin",
     require => [ File["/tmp/vagrant-cache"] ],
     creates => "/tmp/vagrant-cache/${filename}",
