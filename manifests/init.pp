2cf92febf7559ab4181ed96658e0f314ffeff98d
John Brandenburg <jbrandenburg@forumone.com>
2015-05-28 16:00:02 -0400

    iExplicitly uses sh to execute shell script.

--- manifests/init.pp
+++ manifests/init.pp
@@ -30,7 +30,7 @@ class jpeg-optimize::optimize($release = "1.4.3") {
   notify{"/opt/jpegoptim-RELEASE.${release}/configure": }
 
   exec { "jpeg-optimize::optimize::configure":
-    command => "/opt/jpegoptim-RELEASE.${release}/configure",
+    command => "sh opt/jpegoptim-RELEASE.${release}/configure",
     require => "Exec[jpeg-optimize::optimize::extract]",
     creates => "/opt/jpegoptim-RELEASE.${release}/Makefile"
   }
