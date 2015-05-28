cbaa63706ee79d73386bc92fe0296721795b0dbc
John Brandenburg <jbrandenburg@forumone.com>
2015-05-28 11:52:03 -0400

    Spacing.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,31 +4,31 @@ class jpeg-optimize ($release = "1.4.3") {
   $filename = "jpegoptim-RELEASE.${release}.tar.gz"
 
 
-  package {"libjpeg-turbo-devel":
+  package { "libjpeg-turbo-devel":
     ensure => installed,
   }
 
-  exec {"jpeg-optimize::download":
+  exec { "jpeg-optimize::download":
     command => "wget -O ${filename} --directory-prefix=/tmp/vagrant-cache ${url}",
-    path => "/usr/bin/"
-    creates => "/tmp/vagrant-cache/${filename}",
+    path => "/usr/bin/",
+    creates => "/tmp/vagrant-cache/${file:q:name}",
     timeout => 4800,
   }
 
-  exec {"jpeg-optimize::extract":
+  exec { "jpeg-optimize::extract":
     command => "tar -zxvf /tmp/vagrant-cache/${filename} -C /opt",
     path    => "/bin",
     require => [Exec["jpeg-optimize::download"]],
     creates => "/opt/jpegoptim-RELEASE.${release}/README",
   }
 
-  exec {"jpeg-optimize::configure":
+  exec { "jpeg-optimize::configure":
     command => "/opt/jpegoptim-RELEASE.${release}/configure",
     require => "jpeg-optimize::extract",
     creates => "/opt/jpegoptim-RELEASE.${release}/Makefile"
   }
 
-  exec {"jpeg-optimize::make"
+  exec { "jpeg-optimize::make"
     command => "make -C /opt/jpegoptim-RELEASE.${release}/",
     path => "/usr/bin",
     require => "jpeg-optimize::configure",
